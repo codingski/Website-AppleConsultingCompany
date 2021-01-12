777a0eb9a9b746f181a9809f142b3afcce609963
Coding_Chan <69722812+codingski@users.noreply.github.com>
2021-01-12 11:12:08 +0900

    JS: filter work items
    JS: filter work items

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Navbar scroll effect
 const navbar = document.querySelector('#navbar');
 
 document.addEventListener('scroll', () => {
@@ -7,4 +8,25 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('light-color');
     }
+})
+
+
+// Work category filtering
+const workCaregories = document.querySelector('.work__categories');
+const workItem = document.querySelectorAll('.work__item');
+const workItemContainer = document.querySelector('.work__item__container');
+workCaregories.addEventListener('click', (e) => {
+    const target = e.target;
+    const filter = target.dataset.filter || e.target.parentNode.dataset.filter;
+    workItemContainer.classList.add('anim--out');
+    setTimeout(() => {
+        workItem.forEach((item) => {
+            if(filter == '*' || filter == item.dataset.type) {
+                item.classList.remove('invisible');
+            } else {
+                item.classList.add('invisible');
+            }
+        })
+        workItemContainer.classList.remove('anim--out');
+    }, 300);
 })
